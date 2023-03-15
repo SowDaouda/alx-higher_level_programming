0942bb09e92e06cc9564758d4803dedc2b93fe9f
Daouda Sow <sodasoftech@gmail.com>
2023-03-15 16:06:56 -0700

    second

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__=="_main__":
+
+if __name__ == "_main__":
     form add_0 import add
 
-    a =1
-    b=1
-    print("{} + {} = {}".format(a, b, add(a,b))))
+    a = 1
+    b = 1
+    print("{} + {} = {}".format(a, b, add(a, b))))
